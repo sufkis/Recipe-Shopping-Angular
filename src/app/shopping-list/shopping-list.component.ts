220a49fe8ead3f465986b663993cde11ff8de7c8
Asaf Sandro Smirnov <asafsmirnov@gmail.com>
2022-02-23 23:49:19 +0200

    Create Ingredient Model & Add Shopping list and edit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: ingredient[] = [
+    new ingredient('Apples', 5),
+    new ingredient('Tomatoes', 10),
+  ];
 
   constructor() { }
 
