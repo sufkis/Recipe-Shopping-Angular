06107d49393beb1b097aff0828b5d029c629ed5f
Asaf Sandro Smirnov <asafsmirnov@gmail.com>
2022-03-31 00:12:03 +0300

    Fetch data from firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({ providedIn: 'root' })
@@ -10,5 +11,10 @@ export class DataStorageService {
     const recipes = this.recipeService.getRecipes();
     this.http.put('https://ng-course-recipe-book-f8669-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes)
       .subscribe(response => console.log(response));
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://ng-course-recipe-book-f8669-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+      .subscribe(recipes => this.recipeService.setRecipes(recipes));
   }
 }
