1ecaceef2276f1e90ab1b44b7ef59b6c5b07fef3
Asaf Sandro Smirnov <asafsmirnov@gmail.com>
2022-03-31 00:04:11 +0300

    Store data in firebase

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://ng-course-recipe-book-f8669-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes)
+      .subscribe(response => console.log(response));
+  }
+}
